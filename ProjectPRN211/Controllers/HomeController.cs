34f29efcdf045ff204b2533a4ebb238656784ab7
wood-crafter <phanhung2702@gmail.com>
2021-11-07 18:02:02 +0700

    [Init] Index

--- ProjectPRN211/Controllers/HomeController.cs
+++ ProjectPRN211/Controllers/HomeController.cs
@@ -4,35 +4,46 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using ProjectPRN211.Models;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace ProjectPRN211.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private ProjectDemoContext context = new ProjectDemoContext();
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int pageNo)
         {
             if (HttpContext.Session.GetString("UserID") != null)
             {
+                var PAGE_SIZE = 5;
+
                 var userName = JsonConvert.DeserializeObject(HttpContext.Session.GetString("FullName"));
                 TempData["userName"] = userName;
                 var userId = JsonConvert.DeserializeObject(HttpContext.Session.GetString("UserID"));
                 TempData["userId"] = userId;
+
+                TempData["products"] = context.Products
+                    .Skip(pageNo * PAGE_SIZE)
+                    .Take(PAGE_SIZE)
+                    .ToList();
+
+                var pageCount = (int)Math.Ceiling(context.Products.Count() / (double)PAGE_SIZE);
+                TempData["pageCount"] = pageCount;
+                TempData["pageNo"] = pageNo;
+
                 return View();
             }
             else
             {
-                return RedirectToAction("Login");
+                return RedirectToAction("Login", "Account");
             }
         }
 
